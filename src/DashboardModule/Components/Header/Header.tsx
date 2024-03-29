1df7c3e3915f17d81a2ad8845f6be6cb1d58d531
3bdalaEhab <abdalaehab3@gmail.com>
2024-03-29 18:07:51 +0200

    animation

--- src/DashboardModule/Components/Header/Header.tsx
+++ src/DashboardModule/Components/Header/Header.tsx
@@ -2,7 +2,7 @@
 export default function Header() {
   return <>
     <div className="header-container rounded-4 overflow-hidden">
-  <div className="bg-layout h-100 p-sm-5  text-white text-sm-start text-center  d-flex flex-column  justify-content-center">
+  <div className="bg-layout tracking-in-expand-fwd  h-100 p-sm-5  text-white text-sm-start text-center  d-flex flex-column  justify-content-center">
     <h2 >Welcome <span className="main-color">Upskilling</span>
     <p className=" mt-2 ">You can add project and assign tasks to your team</p>
 
